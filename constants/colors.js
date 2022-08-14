fa36285a97cbfa48a485719034883e4f487136a9
木下 拓人 <KinoshitaTakuto@kinoshitatakutonoMacBook-Pro.local>
2022-08-14 16:01:38 +0900

    design-tokensの追加

---
+++ constants/colors.js
@@ -0,0 +1,11 @@
+export const colors = {
+    primary: {
+        dark: "#006064",
+        main: "#0097a7",
+        light: "#b2ebf2",
+    },
+    gray: {
+      main: "#212121",
+      light: "#616161",
+    },
+};
--- pages/pon.js
+++ pages/pon.js
@@ -1,5 +1,10 @@
-import { css } from '@emotion/react'
-const style = css`color: #ff0000;`
+import { css } from "@emotion/react";
+import { colors } from "../constants/colors";
+
+const style = css`
+    background-color: ${colors.primary.main};
+`;
+
 export default function Pon() {
     return(<p css={style}>あけましておめでとう</p>)
 }
